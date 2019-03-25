14cddc7f82f0708de8d1ba26706e47a595b6ea3c
Jonathan Pulido <jonh19h@gmail.com>
2019-03-25 16:19:01 -0600

    adding data base variables

--- model/mysql_connection.php
+++ model/mysql_connection.php
@@ -1,10 +1,11 @@
 <?php
+mysql://b7d25c8d9f22d2:8eb30af5@us-cdbr-iron-east-03.cleardb.net/heroku_d17110f89ab0fc8?reconnect=true
 	class Conection {
 		public function get_conection(){
-			$user = "root";
-			$pass = "";
-			$host = "localhost";
-			$db = "test";
+			$user = "b7d25c8d9f22d2";
+			$pass = "8eb30af5";
+			$host = "us-cdbr-iron-east-03.cleardb.net";
+			$db = "heroku_d17110f89ab0fc8";
 			$conection = new PDO ("mysql:host=$host;dbname=$db;",$user, $pass);
 			return $conection;
 		}
