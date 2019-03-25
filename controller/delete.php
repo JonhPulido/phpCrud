eac954e6e9cc21966d59049658fc37aed93101c6
Jonathan Pulido <jonh19h@gmail.com>
2019-03-25 16:51:35 -0600

    ...

--- controller/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once('../model/mysql_connection.php');
-	require_once('../model/CRUD.php');
-		
-		$id = $_POST['id'];
-		$querys = new Modifiers();
-		$mensaje = $querys->delete_contact($id);
-		echo $mensaje;
-?>
